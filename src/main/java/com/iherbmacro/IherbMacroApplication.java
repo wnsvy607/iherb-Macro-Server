8de2423e9ade9ac370d80c0e81291ff56e4142a9
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-10-01 21:08:37 +0900

    initial commit

---
+++ src/main/java/com/iherbmacro/IherbMacroApplication.java
@@ -0,0 +1,13 @@
+package com.iherbmacro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IherbMacroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IherbMacroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iherbmacro/IherbMacroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iherbmacro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IherbMacroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
